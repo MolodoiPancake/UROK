92b7b8b2f8318ace84bfdfb0e78f8bbf25e4e43f
Gorik <gosha_qwerty@rambler.ru>
2022-11-30 00:28:46 +0800

    Начал делать Task 4

---
+++ OneDrive/Рабочий стол/Geekbrains/Seminars2/Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task 4 Не закончено
+Console.Clear();
+Console.Write("Введите первое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (n > m);
+    Console.WriteLine(n);
+ if (m > b)
+    Console.WriteLine(m);
+ if (b > m)
+    Console.WriteLine(b);
